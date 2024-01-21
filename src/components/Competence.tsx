b22501f43d6d535ece35ac363a9b97e653e7e12a
kendyvincent <vincentkendy@gmail.com>
2024-01-21 22:58:38 +0200

    Shortened competencies and added work history

--- src/components/Competence.tsx
+++ src/components/Competence.tsx
@@ -11,21 +11,21 @@ const arr = [
     icon: <Icon1 />,
     background: "#F2F7F6",
     title: "DESIGN ORIENTED",
-    text: "I specialize in developing visually engaging and user-friendly web applications with a commitment to user-centric design principles. Proficient in UI/UX, I guarantee responsive interfaces, meticulous attention to detail, and smooth collaboration with fellow designers. My goal is to elevate the overall user experience by integrating coding expertise with a keen focus on aesthetic and usability elements.",
+    text: "I specialize in creating visually engaging and user-friendly web applications with a commitment to user-centric design. Proficient in UI/UX, I ensure responsive interfaces, meticulous attention to detail, and seamless collaboration with fellow designers, aiming to elevate the overall user experience.",
   },
   {
     id: 2,
     icon: <Icon2 />,
     background: "#F3F3F3",
     title: "ADAPTABILITY",
-    text: "I possess the ability to navigate and thrive in an ever-changing technological landscape, seamlessly adjusting to new tools, methodologies, and project requirements. I am an adaptable developer that embraces change with enthusiasm, understanding that technology evolves rapidly. When faced with shifting priorities or emerging technologies, I pivot gracefully, leveraging diverse skillset to meet the demands.",
+    text: "I navigate and thrive in a dynamic technological landscape, seamlessly adapting to new tools, methodologies, and project requirements. As an adaptable developer, I embrace change with enthusiasm and pivot gracefully to meet evolving demands, leveraging my diverse skillset in response to shifting priorities or emerging technologies.",
   },
   {
     id: 3,
     icon: <Icon3 />,
     background: "#F9F9FF",
     title: "TEST DRIVEN DEVELOPMENT",
-    text: "A web developer proficient in Test-Driven Development (TDD), adhering to the disciplined TDD cycle, crafting comprehensive tests, and automating testing processes to detect regressions early. I excel in integration testing, collaborate seamlessly with QA teams, and maintain test suites as the codebase evolves. I embrace the Red-Green-Refactor philosophy, and prioritise writing failing tests, implementing minimal code for passing tests, and confidently refactoring code.",
+    text: "As a TDD-proficient web developer, I follow the disciplined TDD cycle, create comprehensive tests for early regression detection, and maintain evolving test suites. Experienced in integration testing and collaborative QA engagement, I embrace the Red-Green-Refactor philosophy by prioritizing failing tests, minimal code for passing tests, and confident refactoring.",
   },
 ];
 
@@ -77,7 +77,7 @@ const Competence: React.FunctionComponent = () => {
         Competence
       </h3>
       <CardsWrapper>
-        {arr.map(({ id, icon, text, background }) => (
+        {arr.map(({ id, icon, text, title, background }) => (
           <ItemCard
             key={id}
             className={css`
@@ -99,6 +99,7 @@ const Competence: React.FunctionComponent = () => {
               `}
             >
               {icon}
+              {/* <div>{title}</div> */}
             </div>
             <Text>{text}</Text>
           </ItemCard>
--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,82 +1,88 @@
-import React from 'react';
-import Tabs from "./Tabs"
-import { css } from "@emotion/css"
-
+import React from "react";
+import Tabs from "./Tabs";
+import { css } from "@emotion/css";
 
 const TabsNavigation = () => {
-    return (
-        <div className={css`
+  return (
+    <div
+      className={css`
         padding: 4rem 12vw;
 
         @media (max-width: 767.98px) {
           padding: 2rem 1rem;
         }
-        `}>
-            <h3 className={css`
-                font-family: 'Josefin Sans', sans serif;
-                font-size: clamp(30px, 4vw, 40px);
-                text-transform: uppercase;
-                font-weight: 700;
-                color: #3B4754;
-                margin-bottom: 5rem;
-                padding-left: 15rem;
+      `}
+    >
+      <h3
+        className={css`
+          font-family: "Josefin Sans", sans serif;
+          font-size: clamp(30px, 4vw, 40px);
+          text-transform: uppercase;
+          font-weight: 700;
+          color: #3b4754;
+          margin-bottom: 5rem;
+          padding-left: 15rem;
 
-                @media (max-width: 767.98px) {
-                  padding-left: 2rem;
-                }
-            `}> Where I&apos;ve worked</h3>
-        <Tabs>
-            <div title="Katana">
-              <h3>Software Engineer @Katana</h3>
-              <h5>Aug 2018 - Present</h5>
-              <p>
-                The quick, brown fox jumps over a lazy dog. DJs flock by when
-                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
-                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
-                The quick, brown fox jumps over a lazy dog. DJs flock by when
-                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
-                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
-              </p>
-            </div>
-            <div title="Optimaplay OU">
-              <h3>Software Engineer @Optimaplay OU</h3>
-              <h5>Aug 2018 - Present</h5>
-              <p>
-                The quick, brown fox jumps over a lazy dog. DJs flock by when
-                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
-                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
-                The quick, brown fox jumps over a lazy dog. DJs flock by when
-                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
-                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
-              </p>
-            </div>
-            <div title="Namespace">
-              <h3>Software Engineer @Namespace</h3>
-              <h5>Aug 2018 - Present</h5>
-              <p>
-                The quick, brown fox jumps over a lazy dog. DJs flock by when
-                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
-                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
-                The quick, brown fox jumps over a lazy dog. DJs flock by when
-                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
-                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
-              </p>
-            </div>
-            <div title="EveryPay">
-              <h3>Software Engineer @EveryPay</h3>
-              <h5>Aug 2018 - Present</h5>
-              <p>
-                The quick, brown fox jumps over a lazy dog. DJs flock by when
-                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
-                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
-                The quick, brown fox jumps over a lazy dog. DJs flock by when
-                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
-                flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex!
-              </p>
-            </div>
-        </Tabs>
+          @media (max-width: 767.98px) {
+            padding-left: 2rem;
+          }
+        `}
+      >
+        {" "}
+        Where I&apos;ve worked
+      </h3>
+      <Tabs>
+        <div title="Kenigbolo Consulting">
+          <h3>Software Engineer @Kenigbolo Consulting</h3>
+          <h5>Aug 2023 - Present</h5>
+          <p>
+            Collaborating cross-functionally, I transformed core frameworks with
+            proficiency in React and TypeScript, ensuring consistent design
+            across platforms, adapting to emerging technologies, and emphasizing
+            comprehensive browser testing. I integrated front-end components,
+            contributed to database design, and implemented responsive design
+            principles to optimize website speed, participating in code reviews
+            for quality assurance and stability.
+          </p>
+        </div>
+        <div title="Integrify">
+          <h3>Software Engineer @Integrify Software Academy</h3>
+          <h5>Aug 2023 - Present</h5>
+          <p>
+            As a freelance developer for a software academy, I bring diverse
+            expertise in programming languages and frameworks. Committed to
+            continuous learning and staying updated on industry trends, I
+            contribute to innovative software solutions. My goal is not only to
+            deliver high-quality code but also to mentor and empower aspiring
+            developers through the academy&rsquo;s educational initiatives.
+          </p>
+        </div>
+        <div title="Namespace">
+          <h3>Software Engineer @Namespace</h3>
+          <h5>Aug 2018 - Present</h5>
+          <p>
+            The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax
+            quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick
+            quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! The quick,
+            brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog.
+            Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex
+            nymphs. Waltz, bad nymph, for quick jigs vex!
+          </p>
+        </div>
+        <div title="Bolt Operations">
+          <h3>Software Engineer @Bolt Operations OÚ</h3>
+          <h5>Aug 2020 - 2022</h5>
+          <p>
+            Responsible for monitoring and managing data operations in a
+            ride-hailing company, ensuring timely and reliable information for
+            customers. Focused on data analysis, identified operational trends,
+            and areas for improvement, contributing to enhanced operational
+            efficiency and an optimal ride-hailing experience.
+          </p>
+        </div>
+      </Tabs>
     </div>
-    );
-}
+  );
+};
 
 export default TabsNavigation;
